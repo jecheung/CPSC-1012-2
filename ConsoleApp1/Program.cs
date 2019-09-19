396ed57ec7e90c1bea6751731f77d845d9a289f9
jecheung <33101511+jecheung@users.noreply.github.com>
2019-09-18 23:30:22 -0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double invested, principalAmount, annualIR, compoundP, years;
+            double compoundY, rateCompound, percentIR;
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("| Compound Interest App |");
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("\n This program is used to calulate the future investment value.");
+
+            Console.WriteLine("Enter investment amount: ");
+            principalAmount = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter annual interest rate in percentage: ");
+            annualIR = double.Parse(Console.ReadLine());
+            percentIR = annualIR / 100;
+
+            Console.WriteLine("Compound periods can be 1 for yearly, 2 for semi-annually, 4 for quarterly, or 12 for monthly");
+            Console.WriteLine("Enter number of compound periods (1, 2, 4, 12): ");
+            compoundP = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of years: ");
+            years = double.Parse(Console.ReadLine());
+
+            compoundY = compoundP * years;
+            rateCompound = percentIR / compoundP;
+
+            invested = principalAmount * Math.Pow((1 + rateCompound),compoundY);
+            
+            Console.WriteLine($"\n Future Value is {invested:C}");
+            Console.WriteLine($"Investment amount: {principalAmount:C}");
+            Console.WriteLine($"Annual Interest Rate: {annualIR:n}%");
+            Console.WriteLine($"Compounds per year: {compoundP}");
         }
     }
 }
