5914ad53981445e81ffd1069c4a846db5a81b001
Jordan Cheung <jcheung8@nait.ca>
2019-09-24 11:45:18 -0600

    made Structure

---
+++ CorePortfolio02-JordanCheung/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Purpose:        To calculate the balance of the account after a specified number years with accumulated interest over time that has been deposited into a saving account
+
+Input:          investment amount, annual interest rate in percentage, compound periods, number of years
+
+Output:         Future value, investmnet amount, annual interest rate, compounds per year
+
+Author:         Jordan Errol Cheung
+
+Last modified:  2019.09.24
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePortfolio02_JordanCheung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("| Flight Time Estimate |");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
