25486b67e19a32c40785ead5fba5d7b5a7438227
Jordan Cheung <jcheung8@nait.ca>
2019-09-26 11:47:13 -0600

    added if statements

--- CorePortfolio02-JordanCheung/Program.cs
+++ CorePortfolio02-JordanCheung/Program.cs
@@ -1,13 +1,13 @@
 ﻿/*
-Purpose:        To calculate the balance of the account after a specified number years with accumulated interest over time that has been deposited into a saving account
+Purpose:        To calculate the overestimate or underestimate if the estimated flight time is too large or too small to the actual flight time
 
-Input:          investment amount, annual interest rate in percentage, compound periods, number of years
+Input:          Estimated flight time in minutes, actual flight time in minutes
 
-Output:         Future value, investmnet amount, annual interest rate, compounds per year
+Output:         Estimated time acceptability or estimated time too large or small and by how much
 
 Author:         Jordan Errol Cheung
 
-Last modified:  2019.09.24
+Last modified:  2019.09.26
 */
 
 using System;
@@ -25,6 +25,50 @@ namespace CorePortfolio02_JordanCheung
             Console.WriteLine("------------------------");
             Console.WriteLine("| Flight Time Estimate |");
             Console.WriteLine("------------------------");
+            Console.WriteLine("This program is used to determine if the estimated flight time is acceptable.");
+
+            int estimatedFT, actualFT, difference, acceptError = 0, estimatedTime;
+
+            Console.Write("Enter estimated flight time in minutes: ");
+            estimatedFT = int.Parse(Console.ReadLine());
+            Console.Write("Enter actual flight time in minutes: ");
+            actualFT = int.Parse(Console.ReadLine());
+
+            if (estimatedFT >= 0 && estimatedFT <= 29)
+                acceptError = 1;
+            else if (estimatedFT >= 30 && estimatedFT <= 59)
+                acceptError = 2;
+            else if (estimatedFT >= 60 && estimatedFT <= 89)
+                acceptError = 3;
+            else if (estimatedFT >= 90 && estimatedFT <= 119)
+                acceptError = 4;
+            else if (estimatedFT >= 120 && estimatedFT <= 179)
+                acceptError = 6;
+            else if (estimatedFT >= 180 && estimatedFT <= 239)
+                acceptError = 8;
+            else if (estimatedFT >= 240 && estimatedFT <= 359)
+                acceptError = 13;
+            else if (estimatedFT >= 360)
+                acceptError = 17;
+            else 
+                Console.WriteLine("Invalid Input");
+
+            difference = estimatedFT - actualFT;
+            //fix this it is going pass the first if
+            if (difference >= acceptError && difference <= acceptError)
+                Console.WriteLine("Estimated time is acceptable");
+            else if (difference > acceptError){
+                estimatedTime = difference - acceptError;
+                Console.WriteLine($"Estimated time too large (by {estimatedTime} minutes)");
+                }
+            else if (difference < acceptError ){
+                estimatedTime = Math.Abs(difference + acceptError);
+                Console.WriteLine($"Estimated time too small (by {estimatedTime} minutes)");
+                }
+            else
+                {
+                Console.WriteLine("Invalid input");
+                }
         }
     }
 }
