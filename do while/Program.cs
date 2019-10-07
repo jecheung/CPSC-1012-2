9b3c9cd56ba7a4d464a20a25b05dbd9b4088f8c2
Jordan Cheung <jcheung8@nait.ca>
2019-10-07 11:34:17 -0600

    do while

---
+++ do while/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents = 0;
+            double curMark = 0;
+            double minMark = 101;
+            double maxMark = -1;
+            double sumOfMarks = 0;
+            double avgMark;
+
+            // Grab our input
+            do
+            {
+                Console.Write("Enter a grade for student or 999 to exit: ");
+                curMark = double.Parse(Console.ReadLine());
+                if (curMark >= 0 && curMark <= 100)
+                {
+                    if (curMark < minMark)
+                        minMark = curMark;
+                    if (curMark > maxMark)
+                        maxMark = curMark;
+                    sumOfMarks += curMark;
+                    numStudents++;
+                }
+                else if (curMark == 999)
+                {
+                    if (sumOfMarks != 0)
+                    {
+                        avgMark = sumOfMarks / (double)numStudents;
+                        Console.WriteLine($"\nAverage Mark: {avgMark}, Highest Mark: {maxMark}, Lowest Mark: {minMark}");
+                    }
+                }
+                else
+                    Console.WriteLine("Invalid Mark entered");
+            } while (curMark != 999);
+
+        }
+    }
+}
---
+++ dowhile2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total, num = 0;
+            bool play = false;
+            Console.WriteLine("***Welcome to the Adding Machine.***");
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Enter = when done.");
+                    Console.Write("Enter a number: ");
+                    num = double.Parse(Console.ReadLine());
+
+                } while (num != '=');
+            } while (play != true);
+        }
+    }
+}
