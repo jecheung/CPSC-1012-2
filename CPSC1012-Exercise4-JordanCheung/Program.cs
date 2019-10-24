a189dc3dfd98f375a2342c079dea74c9a0173403
Jordan Cheung <jcheung8@nait.ca>
2019-10-24 11:35:07 -0600

    added calculations, test prints, comparing

---
+++ CPSC1012-Exercise4-JordanCheung/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC1012_Exercise4_JordanCheung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double interestRatePercent, interestRate, balance;
+            int totalYears, year;
+
+            Console.Write("Enter amount deposited(in dollars): ");
+            int principal = int.Parse(Console.ReadLine());
+            Console.Write("Enter year(s) to invest: ");
+            totalYears = int.Parse(Console.ReadLine());
+            Console.Write("Enter the interest rate percentage: ");
+            interestRatePercent = double.Parse(Console.ReadLine());
+
+            interestRate = interestRatePercent / 100;
+
+            Console.WriteLine("Year\tBalance");
+            for (int i = 1; i <= totalYears; i++)
+            {
+                year = i;
+                balance = principal * Math.Pow((1 + interestRate),year);
+                Console.WriteLine($"{year}\t{balance:c}");
+            }
+        }
+    }
+}
--- CorePortfolio03-JordanCheung/Program.cs
+++ CorePortfolio03-JordanCheung/Program.cs
@@ -30,44 +30,114 @@ namespace CorePortfolio03_JordanCheung
             Console.WriteLine("--------------------------");
             Console.WriteLine("This program simulators a slot machine.");
 
-            int amount = 0;
-            int net, totalWon, totalDeposited;
-            Random rnd = new Random();
-            int ranNum1 = rnd.Next(0, 6);
-            int ranNum2 = rnd.Next(0, 6);
-            int ranNum3 = rnd.Next(0, 6);
-            string word1 = "", word2 = " ", word3 = " ";
-            for (int i = 1; i <= 3; i++)
-            {
+            double amount = 0;
+            double net, totalWon = 0, totalDeposited = 0;
+            double winAmount;
+            string slot1 = "", slot2 = " ", slot3 = " ";
+            string msg, playAgain = "yes";
+            
+            do {
+                Console.Write("Enter the amount to deposit into the slot machine: ");
+                amount = int.Parse(Console.ReadLine());
+                totalDeposited = totalDeposited + amount;
+                Random rnd = new Random();
+                int ranNum1 = rnd.Next(0, 6);
+                int ranNum2 = rnd.Next(0, 6);
+                int ranNum3 = rnd.Next(0, 6);
                 switch (ranNum1)
                 {
                     case 0:
-                        word1 = "Cherries";
+                        slot1 = "Cherries";
                         break;
                     case 1:
-                        word1 = "Oranges";
+                        slot1 = "Oranges";
                         break;
                     case 2:
-                        word1 = "Plums";
+                        slot1 = "Plums";
                         break;
                     case 3:
-                        word1 = "Bells";
+                        slot1 = "Bells";
                         break;
                     case 4:
-                        word1 = "Melons";
+                        slot1 = "Melons";
                         break;
                     case 5:
-                        word1 = "Bars";
+                        slot1 = "Bars";
                         break;
                 }
-            //need to find a easier way to random the numbers
-            //get the word then save the amount deposited, ask for play again everytime after the play, error messages, 2x for two copies 3x for three copies
-            }
-            Console.Write("Enter the amount to deposit into the slot machine: ");
-            amount = int.Parse(Console.ReadLine());
+                switch (ranNum2)
+                {
+                    case 0:
+                        slot2 = "Cherries";
+                        break;
+                    case 1:
+                        slot2 = "Oranges";
+                        break;
+                    case 2:
+                        slot2 = "Plums";
+                        break;
+                    case 3:
+                        slot2 = "Bells";
+                        break;
+                    case 4:
+                        slot2 = "Melons";
+                        break;
+                    case 5:
+                        slot2 = "Bars";
+                        break;
+                }
+                switch (ranNum3)
+                {
+                    case 0:
+                        slot3 = "Cherries";
+                        break;
+                    case 1:
+                        slot3 = "Oranges";
+                        break;
+                    case 2:
+                        slot3 = "Plums";
+                        break;
+                    case 3:
+                        slot3 = "Bells";
+                        break;
+                    case 4:
+                        slot3 = "Melons";
+                        break;
+                    case 5:
+                        slot3 = "Bars";
+                        break;
+                }
+                Console.WriteLine($"{slot1} {slot2} {slot3}");
+                totalDeposited = totalDeposited + amount;
+                if (slot1 == slot2 && slot1 == slot3)
+                {
+                    winAmount = amount * 3;
+                    totalWon = totalWon + winAmount;
+                    msg = "Three words match. You won 3x.";
+                }
+                if (slot1 == slot2 || slot1 == slot3 || slot2 == slot3)
+                {
+                    winAmount = amount * 2;
+                    totalWon = totalWon + winAmount;
+                    msg = "Two words match. You won 2x.";
+                }
+                else 
+                    amount = 0;
+                    msg = "No words match. You won $0";
 
-
-
+                net = totalWon - totalDeposited;
+                if (amount == 0){
+                    Console.Write("Do you want to play again (yes|no)? ");
+                    playAgain = Console.ReadLine();
+                }
+                Console.WriteLine($"totald{totalDeposited} totalwon{totalWon} net{net}");
+            }while (playAgain != "no");
+            
+                //need to find a easier way to random the numbers
+                //ask for play again everytime after the play, error messages, fix net, fix calculations
+                
         }
+            
+
     }
 }
