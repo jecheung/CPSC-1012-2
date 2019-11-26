3feb7c6b43b498cfb0f0eb36eecb357e21698a8b
Jordan Cheung <jcheung8@nait.ca>
2019-11-26 11:23:25 -0700

    work on compare

--- AdvancedPortfolio01-JordanCheung/Program.cs
+++ AdvancedPortfolio01-JordanCheung/Program.cs
@@ -21,63 +21,111 @@ namespace AdvancedPortfolio01_JordanCheung
         {
             Random randNum = new Random();
             int newRandNum = 0;
-            string fname = Path.Combine(Directory.GetCurrentDirectory(), "test.txt");
+            string myDir = "temp";
+            string myFile = "test.txt";
+            string myDirPath = @"C:\temp";
+            string path = @"C:\temp\test.txt";
+
+            Console.WriteLine($"CurrentDirectory: {Environment.CurrentDirectory}\n");
+            Console.WriteLine($"SystemDirectory: {Environment.SystemDirectory}\n");
+            Console.WriteLine($"TempDirectory: {Environment.GetEnvironmentVariable("TEMP")}\n");
+
+            //string fname = Path.Combine(Directory.GetCurrentDirectory(), "test.txt");
+            if(Directory.Exists(myDirPath))
+            {
+                Console.WriteLine($"{myDirPath} is a valid directory\n");
+            }
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("izone");
+                    sw.WriteLine("yena");
+                    sw.WriteLine("sakura");
+                    sw.WriteLine("minju");
+                    sw.WriteLine("yuri");
+                    sw.WriteLine("nako");
+                    sw.WriteLine("hitomi");
+                    sw.WriteLine("eunbi");
+                    sw.WriteLine("chaewon");
+                    sw.WriteLine("chaeyeon");
+                    sw.WriteLine("wonyoung");
+                    sw.WriteLine("yujin");
+                    sw.WriteLine("hyewon");
+
+                }
+                
+            }
+
             List<string> wordsInFile = new List<string>();
             List<char> charsInWord = new List<char>();
+            int wordCount = 0;
             char hiddenChar = '*'; 
             bool WordIsHidden = true; //keep word as *
-            int wordCount = 0;
+            
             var guessed = new List<char>(); //need a list for * to fill with correct characters
             var retry = true; //play again?
 
             // check if File exists
-            if (File.Exists(fname))
+            if (File.Exists(path))
             {
-                Console.WriteLine("CurrentDirectory: " + Environment.CurrentDirectory);
-                Console.WriteLine("SystemDirectory: " + Environment.SystemDirectory);
-                Console.WriteLine("TempDirectory: " + Environment.GetEnvironmentVariable("TEMP"));
-                StreamReader reader = new StreamReader(fname);
+                
+                StreamReader reader = new StreamReader(path);
                 string curLine;
                 while ((curLine = reader.ReadLine()) != null)
-                {
+                { 
                     wordsInFile.Add(curLine);
                 }
                 reader.Close();
-                foreach(var c in charsInWord)
-                {
-                    if (guessed.Contains(c))
-                        Console.Write(c);
-                    else
-                        Console.Write(hiddenChar);
-                }
-                Console.Write("\n(Guess) Enter a Letter in word ");
-                foreach(var c in guessed)
-                {
-                    Console.Write($"{c} >" );
-                }
-                char letter = char.Parse(Console.ReadLine());
-                for (int i = 0; i < wordsInFile.Count; i++)
+                for(int i = 0; i < wordsInFile.Count(); i++)
                 {
                     Console.WriteLine($"word index: {i}, word: {wordsInFile[i]}");
                 }
-                newRandNum = randNum.Next(0, wordsInFile.Count - 1);
-                Console.WriteLine($"Random word index: {newRandNum}");
+                Console.WriteLine($"");
+                newRandNum = randNum.Next(0, wordsInFile.Count() - 1);
+                Console.WriteLine($"Random word index: {newRandNum}\n");
                 string data = wordsInFile[newRandNum];
                 charsInWord.AddRange(data);
-                for (int i = 0; i < charsInWord.Count; i++)
+                for (int i = 0; i < charsInWord.Count(); i++)
                 {
                     Console.WriteLine($"char index: {i}, char: {charsInWord[i]}");
                 }
                 Console.WriteLine("");
-                for (int i = 0; i < charsInWord.Count; i++)
+
+               for (int i = 0; i < charsInWord.Count()-1; i++)
                 {
-                    Console.Write($"{charsInWord[i]}");
+                    guessed.Add(hiddenChar);
                 }
                 Console.WriteLine("");
+
+                Console.Write("\n(Guess) Enter a Letter in word ");
+                for (int i = 0; i < guessed.Count(); i++)
+                {
+                    Console.Write($"{guessed[i]}" );
+                }
+                Console.Write(" > ");
+                char letter = char.Parse(Console.ReadLine());
+                if (charsInWord.Contains(letter))
+                {
+                    
+                }
+                for (int i = 0; i < charsInWord.Count()-1; i++)
+                {
+                    if (charsInWord[i] == char)
+                    else
+                        Console.Write(hiddenChar);
+                }
+
             }
             else
             {
-                Console.WriteLine($"The filename {fname} does not exist.");
+                Console.WriteLine($"The filename {path} does not exist.");
             }
         }
     }
